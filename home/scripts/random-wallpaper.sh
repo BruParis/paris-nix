f6333801ed6f0a254290fe8fb61c4fa8664aea80
BrnPrs <parisbruno85@gmail.com>
2025-03-02 10:30:03 +0100

    use /dev/urandom is random_wallpaper.sh

--- home/scripts/random-wallpaper.sh
+++ home/scripts/random-wallpaper.sh
@@ -3,7 +3,7 @@
 WALLPAPER_DIRS="$HOME/.config/hypr/images $HOME/Documents/Wallpapers"
 
 # Find a random wallpaper
-WALLPAPER=$(find $WALLPAPER_DIRS -type f -name "*.jpg" -o -name "*.png" | shuf -n 1)
+WALLPAPER=$(find $WALLPAPER_DIRS -type f -name "*.jpg" -o -name "*.png" | shuf --random-source=/dev/urandom -n 1)
 
 swww img $WALLPAPER --transition-type none --resize fit
 echo $WALLPAPER
