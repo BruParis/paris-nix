e1a9edb292516a46745eb4acb8c86b5dab282375
BrnPrs <parisbruno85@gmail.com>
2025-06-24 22:17:26 +0200

    nix flake update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
