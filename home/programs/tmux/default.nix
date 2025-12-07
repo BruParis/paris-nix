1087a46539173972d5b415ed773cd6f8539d75fd
BrnPrs <parisbruno85@gmail.com>
2025-12-07 13:18:44 +0100

    add tmux custom bind-keys

--- home/programs/tmux/default.nix
+++ home/programs/tmux/default.nix
@@ -16,6 +16,13 @@
       bind-key -T copy-mode-vi 'y' send -X copy-selection-and-cancel
       set -g default-terminal "xterm-256color"
       set -ga terminal-overrides ",xterm-256color:Tc"
+
+      # Custom keybindings for panel navigation
+      bind-key -r -T prefix h select-pane -L
+      bind-key -r -T prefix j select-pane -D
+      bind-key -r -T prefix k select-pane -U
+      bind-key -r -T prefix l select-pane -R
+
     '';
   };
 }
